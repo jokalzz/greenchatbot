01c71812d7229c958bd3477c8f06cc6237e5e766
jokalzz <jokakalzz@gmail.com>
2026-01-17 00:20:22 +0800

    setting up to deploy

--- server.js
+++ server.js
@@ -87,6 +87,12 @@ Respons harus terstruktur dengan baik dan mudah dibaca.`;
   }
 });
 
-app.listen(3000, () => {
-  console.log("✅ Server jalan di http://localhost:3000");
-});
+
+const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+    console.log(`✅ Server jalan di http://localhost:${PORT}`);
+  });
+}
+
+export default app;
